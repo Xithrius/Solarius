d7b8dbbb5b53411c96126ee3264d93ab891c5ee7
Xithrius <xithrius@gmail.com>
2024-01-20 23:58:27 -0800

    Downgraded to Python 3.11

--- api/app/routers/search/utils.py
+++ api/app/routers/search/utils.py
@@ -1,15 +1,12 @@
 from typing import Any
 
-# from transformers import pipeline
+from transformers import pipeline
 
 
 def summarize_search_snippets(text: str) -> Any:
-    return None
+    summarizer = pipeline("summarization", model="t5-base")
+    summary = summarizer(text, max_length=50, min_length=10, do_sample=False)
 
+    summary_text = summary[0]["summary_text"]
 
-#     summarizer = pipeline("summarization", model="t5-base")
-#     summary = summarizer(text, max_length=50, min_length=10, do_sample=False)
-
-#     summary_text = summary[0]["summary_text"]
-
-#     return summary_text
+    return summary_text
